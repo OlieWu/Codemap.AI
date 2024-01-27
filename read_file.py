1cde0e9a18b5063004dcf0b068bd856f0b51a82e
tghong <tghong@umich.edu>
2024-01-27 13:26:58 -0500

    Reading files fixed

--- read_file.py
+++ read_file.py
@@ -1,14 +1,16 @@
 import sys
+import os
+
 
 def read_file(file):
-"""Helper function to read contents of an individual file."""
+    """Helper function to read contents of an individual file."""
     with open(file, 'r') as f:
         file_content = f.read()
     return file_content
 
 
 def aggregate_files(directory, ignore):
-"""Function that goes through all the files in a directory."""
+    """Function that goes through all the files in a directory."""
     file_list = []
     for root, subdirs, files in os.walk(directory):
         for f in files:
@@ -22,11 +24,12 @@ def main():
     target_directory = sys.argv[1]
     ignored_files = sys.argv[3:]
 
-    files = aggergate_files(target_directory, ignored_files)
-    with open(output_file, 'w') as output:
+    files = aggregate_files(target_directory, ignored_files)
+    # print(files)
+    with open("output.txt", 'w') as output:
         for f in files:
-            content = read_file(f)
-            output.write(f"Content of {f}:\n{content}\n\n")
+            content = read_file(os.path.join(target_directory, f))
+            output.write(f"Content of {os.path.join(target_directory, f)}:\n{content}\n\n")
 
 
 if __name__ == "__main__":
